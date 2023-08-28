e643cff03dbbdbfc2b76168d996632c4b2949630
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-08-28 17:02:30 -0300

    In this update was made possible to select a game by Id

--- src/main/java/com/drakk/gamelist/controllers/GameController.java
+++ src/main/java/com/drakk/gamelist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.drakk.gamelist.controllers;
 
 
+import com.drakk.gamelist.dto.GameDTO;
 import com.drakk.gamelist.dto.GameMinDTO;
 import com.drakk.gamelist.entities.Game;
 import com.drakk.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
 
 
--- src/main/java/com/drakk/gamelist/services/GameService.java
+++ src/main/java/com/drakk/gamelist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.drakk.gamelist.services;
 
+import com.drakk.gamelist.dto.GameDTO;
 import com.drakk.gamelist.dto.GameMinDTO;
 import com.drakk.gamelist.entities.Game;
 import com.drakk.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,7 +16,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
 
+    }
+
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
 
