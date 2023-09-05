c45420b53e72a4090e2afc2e8569bf2400ded525
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-09-05 17:00:46 -0300

    Added Projections - SQL search - GameMinProjection

--- src/main/java/com/drakk/gamelist/dto/GameMinDTO.java
+++ src/main/java/com/drakk/gamelist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.drakk.gamelist.dto;
 
 import com.drakk.gamelist.entities.Game;
+import com.drakk.gamelist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -22,6 +23,13 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/drakk/gamelist/services/GameService.java
+++ src/main/java/com/drakk/gamelist/services/GameService.java
@@ -3,6 +3,7 @@ package com.drakk.gamelist.services;
 import com.drakk.gamelist.dto.GameDTO;
 import com.drakk.gamelist.dto.GameMinDTO;
 import com.drakk.gamelist.entities.Game;
+import com.drakk.gamelist.projections.GameMinProjection;
 import com.drakk.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,14 @@ public class GameService {
         List<Game> result = gameRepository.findAll();
 
          return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList (Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
 
 
     }
